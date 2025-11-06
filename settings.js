8216b2a4b40e1b5a29f434d3b0c960aee55d08c5
muzii-523 <ghourimubashir60@gmail.com>
2025-11-06 19:06:44 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settings = {
+  packname: 'MUZAMMIL Tech Hub Bot',
+  author: '‎',
+  botName: "A   T   H  Bot",
+  botOwner: 'MuzammilMD Official', // Your name
+  ownerNumber: '923237045919', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "👥public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "> ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ: ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ꜰᴏʀ ᴍᴀɴᴀɢɪɴɢ ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ ᴀɴᴅ ᴀᴜᴛᴏᴍᴀᴛɪɴɢ ᴛᴀꜱᴋꜱ.",
+  caption: "𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 𝙈𝙐𝙕𝘼𝙈𝙈𝙄𝙇",
+  version: "2.2.6 alpha-8",
+  prefix: ".", // prefix (e.g., ., /, !, *)
+  timezone: "Asia/Karachi", //put your country timeZone....leave blank if u don't know
+  updateZipUrl: "https://api.github.com/muzii-523/MUUZAMMIL-TECH-BOT/main/zip",
+};
+
+module.exports = settings;
